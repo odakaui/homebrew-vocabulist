37e31226ead5a56f12ca5ffc671f7aba4f42f5bb
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2020-07-18 16:11:48 -0700

    Update for Version 0.1.6

--- Formula/vocabulist.rb
+++ Formula/vocabulist.rb
@@ -1,15 +1,15 @@
 class Vocabulist < Formula
   desc "Personalized vocabulary frequency list for learning Japanese"
   homepage "https://github.com/odakaui/vocabulist"
-  version "0.1.4"
+  version "0.1.6"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/odakaui/vocabulist/releases/download/v#{version}/vocabulist-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "7530cae373fc1dfa7fe1a7e1e1b2663c1d9a79c2b6fee784a715de9951c9e6b3"
+    sha256 "0a0d1b97af00621054335629bb868df6b6da7debd0be56f55c81fec21d5d9c8b"
   elsif OS.linux?
     url "https://github.com/odakaui/vocabulist/releases/download/v#{version}/vocabulist-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "e3c74a42ac934c0782a12af5695a5a5baeeecfbee26b6c5e9025c1eca6dc073f"
+    sha256 "2f4117f15418640ed9093b807470e118d7e50602840e3fec2075fb4dbf47452b"
   end
 
   depends_on "mecab" => :recommended
